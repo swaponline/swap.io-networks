93233bbbc49fd38b10263ed660d7314e6973c148
Vitaliy Shulik <shulikvitalya@gmail.com>
2021-08-14 16:08:17 +0600

    change tokenslists order link and change pancakeswap link

--- scripts/constants/externalTokensListsLinks.ts
+++ scripts/constants/externalTokensListsLinks.ts
@@ -1,6 +1,6 @@
 export const externalTokensListsLinks: {[listName: string]: string} = {
-    "borgswap":                     "https://api.borgswap.exchange/tokens.json",
     "pancakeswap extended":         "https://raw.githubusercontent.com/pancakeswap/pancake-toolkit/master/packages/token-lists/lists/pancakeswap-extended.json",
+    "borgswap":                     "https://api.borgswap.exchange/tokens.json",
     "1inch":                        "https://tokens.1inch.eth.link/",
     "zapper token list":            "https://zapper.fi/api/token-list"
 }
