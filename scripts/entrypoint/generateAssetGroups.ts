893d3e4a81c676e39c571ef5f076b3c1401eee57
Vitaliy Shulik <shulikvitalya@gmail.com>
2021-09-25 14:50:55 +0600

    add `AssetGroup` type

--- scripts/entrypoint/generateAssetGroups.ts
+++ scripts/entrypoint/generateAssetGroups.ts
@@ -8,6 +8,62 @@ import {
 } from "../common/filesystem"
 import { readJsonFile, checkFile, writeToFileWithUpdate } from "../common/json"
 
+type UniversalObj = {
+  [key: string]: any
+}
+
+enum AssetTypes {
+  "coin",
+  "token"
+}
+
+type AssetInfo = {
+  name: string
+  symbol: string
+  logo: string
+  type: AssetTypes
+
+  // "coin" type
+  slug?: string
+  name_plural?: string
+  denominator?: number
+
+  // "token" type
+  address?: string
+  decimals?: number
+  chainId?: number
+  tags?: string[]
+}
+
+type AssetNetworkInfo = {
+  name: string
+  slug: string
+  logo: string
+}
+
+type AssetGroup = {
+  symbol: string,
+  name: string,
+  logo: string,
+  priority: number,
+  networks: {
+    network: AssetNetworkInfo
+    assets: AssetInfo[]
+  }[]
+}
+
+type tokenInfo = {
+  name: string,
+  address: string,
+  symbol: string,
+  decimals: number,
+  chainId: number,
+  "logo": string,
+  "tags": string[]
+}
+
+type NetworkTokensListObj = {[tokensID: string]: tokenInfo}
+
 
 const generateAssetGroups = () => {
   const distPath = getAbsolutePath('/dist')
@@ -16,29 +72,48 @@ const generateAssetGroups = () => {
   const networksInfoFileName = 'networksInfo.json'
   const assetGroupsFileName = 'assets-groups.json'
 
-  const mainnetNetworksInfoList = readJsonFile(`${mainnetPath}/${networksInfoFileName}`) as string[]
-  const testnetNetworksInfoList = readJsonFile(`${testnetPath}/${networksInfoFileName}`) as string[]
+  const mainnetNetworksInfoList = readJsonFile(`${mainnetPath}/${networksInfoFileName}`) as UniversalObj[]
+  const testnetNetworksInfoList = readJsonFile(`${testnetPath}/${networksInfoFileName}`) as UniversalObj[]
+
+  const mainnetNetworkInfoBySlug: UniversalObj = {}
+
+  mainnetNetworksInfoList.forEach(networkInfo => {
+    mainnetNetworkInfoBySlug[networkInfo.slug] = networkInfo
+  })
 
   // console.log('mainnetNetworksInfoList', mainnetNetworksInfoList)
   // console.log('testnetNetworksInfoList', testnetNetworksInfoList)
-  const customAssetGroupsInfo = customAssetGroups.map(assetGroup => {
+  const customAssetGroupsInfo: UniversalObj[] = customAssetGroups.map(assetGroup => {
     const assetGroupInfoPath = getAssetGroupInfoPath(assetGroup)
 
     if (isPathExistsSync(assetGroupInfoPath))
       return readJsonFile(assetGroupInfoPath)
   })
 
-  console.log('customAssetGroupsInfo', customAssetGroupsInfo)
+  const mainnetAssetGroups: AssetGroup[] = []
+  const testnetAssetGroups: AssetGroup[] = []
 
-  const mainnetAssetGroups: string[] = []
-  const testnetAssetGroups: string[] = []
+  customAssetGroupsInfo.forEach(assetGroup => {
+    console.log('assetGroup', assetGroup)
+    const mainAssetRelativePath = assetGroup["asset-list"][0]
+    const mainAssetNetworkSlug = getNetworkSlugByAssetRelativePath(mainAssetRelativePath)
+    console.log('mainAssetNetworkSlug', mainAssetNetworkSlug)
+  })
 
-  checkFile(mainnetPath, assetGroupsFileName, [])
-  writeToFileWithUpdate(mainnetPath, assetGroupsFileName, mainnetAssetGroups)
+  // checkFile(mainnetPath, assetGroupsFileName, [])
+  // writeToFileWithUpdate(mainnetPath, assetGroupsFileName, mainnetAssetGroups)
 
 
-  checkFile(testnetPath, assetGroupsFileName, [])
-  writeToFileWithUpdate(testnetPath, assetGroupsFileName, testnetAssetGroups)
+  // checkFile(testnetPath, assetGroupsFileName, [])
+  // writeToFileWithUpdate(testnetPath, assetGroupsFileName, testnetAssetGroups)
 }
+
+const getNetworkSlugByAssetRelativePath = (relativePath: string): string => relativePath.split('/')[2]
+
+const getAssetNetworkInfo = (networkInfo: any, assetInfo: any) => ({
+  name: networkInfo.name,
+  slug: networkInfo.slug,
+  logo: assetInfo.logo
+})
 
 generateAssetGroups()
